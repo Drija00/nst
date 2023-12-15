4b8f1b6b3e853cf4fabfb590fdc818da96d621bf
Drija00 <andrijamaric05@gmail.com>
2023-12-15 10:20:28 +0100

    added to domain model shortname param

---
+++ src/main/java/nst/springboot/restexample01/dto/DepartmentDto.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nst.springboot.restexample01.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ *
+ * @author student2
+ */
+public class DepartmentDto implements Serializable{
+    private Long id;
+    
+    @NotNull
+    @Size(min = 2,max = 10, message = "Broj znakova [2-20]")
+    private String name;
+
+    @NotNull
+    @Size(min = 2,max = 10, message = "Broj znakova [2-10]")
+    private String shortname;
+
+    public DepartmentDto() {
+    }
+
+    public DepartmentDto(Long id, String name, String shortname) {
+        this.id = id;
+        this.name = name;
+        this.shortname = shortname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortname() {
+        return shortname;
+    }
+
+    public void setShortname(String shortname) {
+        this.shortname = shortname;
+    }
+}
